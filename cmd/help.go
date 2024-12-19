f1612ab32e0ae53494c00106bd788b40ee01e4e9
filipeapdo <filipeaparecido@gmail.com>
2024-12-19 15:36:26 -0300

    chore(global): add .gitignore, small change on help command

--- cmd/help.go
+++ cmd/help.go
@@ -6,6 +6,6 @@ func showHelp() {
 	fmt.Println("\t\t\tAVAILABLE COMMMANDS")
 	fmt.Println("\thelp \t\t\t\t- Show this help message")
 	fmt.Println("\tview [month] \t\t\t- View financial data for the given month")
-	fmt.Println("\tadd [month] [day] [income] \t\t\t- Add income to specific day of a month")
+	fmt.Println("\tadd [month] [day] [income] \t- Add income to specific day of a month")
 	fmt.Println("\texit \t\t\t\t- Quit program")
 }
